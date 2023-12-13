ea2de498b75f9753c16a2e10f7ea34df39526a6b
alex <alex@susanu.com>
2023-12-13 00:22:00 +0000

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module lenslocked
+module github.com/alexsusanu/lenslocked
 
-go 1.21
+go 1.21.4
--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8") // Set the content type to text/html
 	fmt.Fprint(w, "<h1>Welcome to Lenslocked!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8") // Set the content type to text/html
+	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:example@example.com\">example@example.com</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Listening on port :3000")
 	http.ListenAndServe(":3000", nil)
 }
