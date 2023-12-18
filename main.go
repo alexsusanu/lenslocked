be4c90b7bcda364dd8d74b6acbc5018c83965d72
alex <alex@susanu.com>
2023-12-18 20:19:20 +0000

    fs

--- main.go
+++ main.go
@@ -3,30 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/alexsusanu/lenslocked/controllers"
+	"github.com/alexsusanu/lenslocked/templates"
 	"github.com/alexsusanu/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 	"net/http"
-	"path/filepath"
 )
 
 func main() {
 	r := chi.NewRouter()
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
--- views/template.go
+++ views/template.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -23,6 +24,24 @@ func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 
 func Parse(filepath string) (Template, error) {
 	htmlTpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("Error parsing template: %v", err)
+		return Template{}, fmt.Errorf("error parsing template: %w", err)
+	}
+	return Template{
+		htmlTpl: htmlTpl,
+	}, nil
+}
+
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func ParseFS(fs fs.FS, pattern string) (Template, error) {
+	htmlTpl, err := template.ParseFS(fs, pattern)
 	if err != nil {
 		log.Printf("Error parsing template: %v", err)
 		return Template{}, fmt.Errorf("error parsing template: %w", err)
